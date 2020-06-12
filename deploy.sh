4200fd89a2da14ca2491df62eab9efa77863ef15
unknown <scott.wilson@evolo.co.uk>
2020-06-12 09:09:37 +0100

    deployment push google cloud

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+docker build -t scowil101/multi-client:latest -t scowil101/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t scowil101/multi-server:latest -t scowil/multi-server:$SHA -f ./server/Dockerfle ./server
+docker build -t scowil101/muti-worker:latest -t scowil101/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker push scowil101/multi-client:latest
+docker push scowil101/multi-server:latest
+docker push scowil101/multi-worker:latest
+docker push scowil101/multi-client:$SHA
+docker push scowil101/multi-server:$SHA
+docker push scowil101/multi-worker:$SHA
+kubectl apply -f k8s
+kubectl set image deployments/server-deployment server=scowil101/multi-server:$SHA
+kubectl set image deployments/client-deployment client=scowil101/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=scowil101/multi-worker:$SHA
+
